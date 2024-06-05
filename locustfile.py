05916105369eb4368de4bf459ee5d162c7cf33a4
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-06-05 15:12:51 +0700

    json_parser

--- locustfile.py
+++ locustfile.py
@@ -9,14 +9,15 @@ def load_har_file(file_path):
     '''Функция для парсинга .har файла'''
     with open(file_path, 'r', encoding='utf-8') as f:
         har_data = json.load(f)
-        print(har_data)
-    return HarParser(har_data)
+    return har_data
+
+
 
 
 class UserStartupium(HttpUser):
     '''Основной класс пользователя'''
-    wait_time = between(5, 6)
-    har_parser = load_har_file('session_startupium.har')
+    # wait_time = between(5, 6)
+    json_parser = load_har_file('all_article.json')
 
 
     @task(1)
@@ -39,23 +40,21 @@ class UserStartupium(HttpUser):
         '''Показать рандомный профиль'''
         self.client.get(f'/profile/{choice([109, 107, 106, 105])}')
 
-    # @task
-    # def run_requests_from_har(self):
-    #     # Проход по всем записям в HAR файле
-    #     for entry in self.har_parser.har_data['log']['entries']:
-    #         request = entry['request']
-    #         method = request['method']
-    #         url = request['url']
-    #         headers = {header['name']: header['value'] for header in request['headers']}
-    #         params = {param['name']: param['value'] for param in request['queryString']}
-    #         post_data = request.get('postData', {}).get('text', '')
-    #
-    #         # Выполняем запросы на основе данных из HAR файла
-    #         if method == 'GET':
-    #             self.client.get(url, headers=headers, params=params)
-    #         elif method == 'POST':
-    #             self.client.post(url, headers=headers, params=params, data=post_data)
-    #         elif method == 'PUT':
-    #             self.client.put(url, headers=headers, params=params, data=post_data)
-    #         elif method == 'DELETE':
-    #             self.client.delete(url, headers=headers, params=params)
+    @task(1)
+    def run_requests_from_har(self):
+        # Проход по всем записям в json файле
+        for entry in self.json_parser:
+            method = entry['method']
+            url = f"{entry['path']}"
+            headers = {header['name']: header['value'] for header in entry['request']['header']['headers']}
+            params = {param['name']: param['value'] for param in entry['query']} if entry['query'] else {}
+            post_data = entry['request'].get('postData', {}).get('text', '')
+
+            if method == 'GET':
+                self.client.get(url, headers=headers, params=params)
+            elif method == 'POST':
+                self.client.post(url, headers=headers, params=params, data=post_data)
+            elif method == 'PUT':
+                self.client.put(url, headers=headers, params=params, data=post_data)
+            elif method == 'DELETE':
+                self.client.delete(url, headers=headers, params=params)
