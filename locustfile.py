70bb5641f0e468e9fe88995dbd133cab96260b8e
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-08-03 12:57:15 +0700

    bz_socket_atata

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,13 @@
 import json
-
-from locust import User, task, between
-from websocket import create_connection
 import ssl
+import time
+from locust import User, task, between
+from locust.exception import RescheduleTask
+from websocket import create_connection, WebSocketConnectionClosedException
 
 
 class AuthUser(User):
-    wait_time = between(5, 6)  # Время ожидания между выполнением задачам от 5 до 6 секунд
+    wait_time = between(5, 6)  # Время ожидания между выполнением задач от 5 до 6 секунд
     data = {
         "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
         "jsonrpc": "2.0",
@@ -30,26 +31,90 @@ class AuthUser(User):
         # Закрытие соединения
         self.ws.close()
 
-    @task(2) # чем больше число в декораторе task, тем задача будет чаще выполнятся в сравнение с другими
+    @task(2)
     def login(self):
         # Сериализация JSON-объекта в строку
-        data_login = self.data
+        data_login = self.data.copy()  # Копируем data, чтобы избежать изменения оригинала
         data_login["method"] = "v1_webappLogin"
         data_login["params"][0]['confirmation_type'] = "sms"
         message_login = json.dumps(data_login)
-        # Отправка сообщения
-        self.ws.send(message_login)
-        # Получение ответа
-        response_login = self.ws.recv()
-        print(f"Received message: {response_login}")
-        data_sms = self.data
-        data_login["method"] = "v1_webappConfirm"
-        data_sms["params"][0]['code'] = "0001"
-        message_sms = json.dumps(data_sms)
-        self.ws.send(message_sms)
-        response_sms = self.ws.recv()
-        print(f"Received message: {response_sms}")
+
+        # Запись времени начала отправки сообщения
+        start_time = time.time()
+
+        try:
+            # Отправка сообщения
+            self.ws.send(message_login)
+
+            # Получение ответа
+            response_login = self.ws.recv()
+
+            # Запись успешного ответа
+            total_time = int((time.time() - start_time) * 1000)  # Время в миллисекундах
+            self.environment.events.request.fire(
+                request_type="WebSocket",
+                name="login",
+                response_time=total_time,
+                response_length=len(response_login),
+                response="Success",
+                exception=None,
+            )
+
+            print(f"Received message: {response_login}")
+
+            # Обработка второго сообщения для подтверждения
+            data_sms = self.data.copy()  # Копируем data, чтобы избежать изменения оригинала
+            data_sms["method"] = "v1_webappConfirm"
+            data_sms["params"][0]['code'] = "0001"
+            message_sms = json.dumps(data_sms)
+
+            # Запись времени начала отправки сообщения
+            start_time = time.time()
+
+            # Отправка сообщения
+            self.ws.send(message_sms)
+
+            # Получение ответа
+            response_sms = self.ws.recv()
+
+            # Запись успешного ответа
+            total_time = int((time.time() - start_time) * 1000)  # Время в миллисекундах
+            self.environment.events.request.fire(
+                request_type="WebSocket",
+                name="confirm",
+                response_time=total_time,
+                response_length=len(response_sms),
+                response="Success",
+                exception=None,
+            )
+
+            print(f"Received message: {response_sms}")
+
+        except WebSocketConnectionClosedException as e:
+            # Запись ошибки, если WebSocket соединение было закрыто
+            total_time = int((time.time() - start_time) * 1000)  # Время в миллисекундах
+            self.environment.events.request.fire(
+                request_type="WebSocket",
+                name="login",
+                response_time=total_time,
+                response_length=0,
+                response="Failure",
+                exception=e,
+            )
+            raise RescheduleTask()  # Перезапланировать задачу для выполнения позже
+
+        except Exception as e:
+            # Запись других ошибок
+            total_time = int((time.time() - start_time) * 1000)  # Время в миллисекундах
+            self.environment.events.request.fire(
+                request_type="WebSocket",
+                name="login",
+                response_time=total_time,
+                response_length=0,
+                response="Failure",
+                exception=e,
+            )
+            raise RescheduleTask()  # Перезапланировать задачу для выполнения позже
 
 # запуск из директории с файлом locustfile.py
 # locust -f locustfile.py
-
