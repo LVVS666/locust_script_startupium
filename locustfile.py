ca1c63248bfa91be86a0fcb92ff4fb6106fa56d4
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-08-03 12:45:09 +0700

    bz_socket_atata

--- locustfile.py
+++ locustfile.py
@@ -4,8 +4,22 @@ from locust import User, task, between
 from websocket import create_connection
 import ssl
 
-class Login(User):
+
+class AuthUser(User):
     wait_time = between(5, 6)  # Время ожидания между выполнением задачам от 5 до 6 секунд
+    data = {
+        "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
+        "jsonrpc": "2.0",
+        "params": [
+            {
+                "phone": "79996661155",
+                "udid": "1a4a9fa8f577915c4d2c31cb20983895",
+                "app_version": "23.0",
+                "platform": "iOS",
+                "os_version": "10.15.7"
+            }
+        ]
+    }
 
     def on_start(self):
         # Инициализация WebSocket-соединения
@@ -18,52 +32,24 @@ class Login(User):
 
     @task(2) # чем больше число в декораторе task, тем задача будет чаще выполнятся в сравнение с другими
     def login(self):
-        data = {
-            "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
-            "jsonrpc": "2.0",
-            "method": "v1_webappLogin",
-            "params": [
-                {
-                    "phone": "79996661155",
-                    "confirmation_type": "sms",
-                    "udid": "1a4a9fa8f577915c4d2c31cb20983895",
-                    "app_version": "23.0",
-                    "platform": "iOS",
-                    "os_version": "10.15.7"
-                }
-            ]
-        }
         # Сериализация JSON-объекта в строку
-        message = json.dumps(data)
+        data_login = self.data
+        data_login["method"] = "v1_webappLogin"
+        data_login["params"][0]['confirmation_type'] = "sms"
+        message_login = json.dumps(data_login)
         # Отправка сообщения
-        self.ws.send(message)
+        self.ws.send(message_login)
         # Получение ответа
-        response = self.ws.recv()
-        print(f"Received message: {response}")
-
-    @task(2)
-    def send_sms(self):
-        data = {
-            "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
-            "jsonrpc": "2.0",
-            "method": "v1_webappLogin",
-            "params": [
-                {
-                    "phone": "79996661155",
-                    "code" : "0001",
-                    "udid": "1a4a9fa8f577915c4d2c31cb20983895",
-                    "app_version": "23.0",
-                    "platform": "iOS",
-                    "os_version": "10.15.7"
-                }
-            ]
-        }
-        message = json.dumps(data)
-        self.ws.send(message)
-        response = self.ws.recv()
-        print(f"Received message: {response}")
-
-
+        response_login = self.ws.recv()
+        print(f"Received message: {response_login}")
+        data_sms = self.data
+        data_login["method"] = "v1_webappConfirm"
+        data_sms["params"][0]['code'] = "0001"
+        message_sms = json.dumps(data_sms)
+        self.ws.send(message_sms)
+        response_sms = self.ws.recv()
+        print(f"Received message: {response_sms}")
 
 # запуск из директории с файлом locustfile.py
 # locust -f locustfile.py
+
