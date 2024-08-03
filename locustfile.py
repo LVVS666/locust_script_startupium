00a905e4285d737a86b3f0a93e9b81553ead7901
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-08-03 12:24:53 +0700

    bz_socket_atata

--- locustfile.py
+++ locustfile.py
@@ -1,60 +1,69 @@
 import json
 
-from locust import task, HttpUser, between
-from random import choice
-from haralyzer import HarParser
+from locust import User, task, between
+from websocket import create_connection
+import ssl
 
+class Login(User):
+    wait_time = between(5, 6)  # Время ожидания между выполнением задачам от 5 до 6 секунд
 
-def load_har_file(file_path):
-    '''Функция для парсинга .har файла'''
-    with open(file_path, 'r', encoding='utf-8') as f:
-        har_data = json.load(f)
-    return har_data
+    def on_start(self):
+        # Инициализация WebSocket-соединения
+        sslopt = {"cert_reqs": ssl.CERT_NONE}
+        self.ws = create_connection("wss://dev3partners.berizaryad.ru/ws", sslopt=sslopt)
 
+    def on_stop(self):
+        # Закрытие соединения
+        self.ws.close()
 
-
-
-class UserStartupium(HttpUser):
-    '''Основной класс пользователя'''
-    # wait_time = between(5, 6)
-    json_parser = load_har_file('all_article.json')
-
-
-    @task(1)
-    def all_projects(self):
-        '''Показать все проекты'''
-        self.client.get('/projects')
+    @task(2) # чем больше число в декораторе task, тем задача будет чаще выполнятся в сравнение с другими
+    def login(self):
+        data = {
+            "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
+            "jsonrpc": "2.0",
+            "method": "v1_webappLogin",
+            "params": [
+                {
+                    "phone": "79996661155",
+                    "confirmation_type": "sms",
+                    "udid": "1a4a9fa8f577915c4d2c31cb20983895",
+                    "app_version": "23.0",
+                    "platform": "iOS",
+                    "os_version": "10.15.7"
+                }
+            ]
+        }
+        # Сериализация JSON-объекта в строку
+        message = json.dumps(data)
+        # Отправка сообщения
+        self.ws.send(message)
+        # Получение ответа
+        response = self.ws.recv()
+        print(f"Received message: {response}")
 
     @task(2)
-    def project(self):
-        '''Показать один рандомный проект'''
-        self.client.get(f'/project/{choice([111, 1245, 123, 23124])}')
+    def send_sms(self):
+        data = {
+            "id": "4dfe08d0-3c88-4867-a425-210da92527f7",
+            "jsonrpc": "2.0",
+            "method": "v1_webappLogin",
+            "params": [
+                {
+                    "phone": "79996661155",
+                    "code" : "0001",
+                    "udid": "1a4a9fa8f577915c4d2c31cb20983895",
+                    "app_version": "23.0",
+                    "platform": "iOS",
+                    "os_version": "10.15.7"
+                }
+            ]
+        }
+        message = json.dumps(data)
+        self.ws.send(message)
+        response = self.ws.recv()
+        print(f"Received message: {response}")
 
-    @task(3)
-    def all_users(self):
-        '''Показать всех пользователей'''
-        self.client.get('/users')
 
-    @task(4)
-    def users(self):
-        '''Показать рандомный профиль'''
-        self.client.get(f'/profile/{choice([109, 107, 106, 105])}')
 
-    @task(1)
-    def run_requests_from_har(self):
-        # Проход по всем записям в json файле
-        for entry in self.json_parser:
-            method = entry['method']
-            url = f"{entry['path']}"
-            headers = {header['name']: header['value'] for header in entry['request']['header']['headers']}
-            params = {param['name']: param['value'] for param in entry['query']} if entry['query'] else {}
-            post_data = entry['request'].get('postData', {}).get('text', '')
-
-            if method == 'GET':
-                self.client.get(url, headers=headers, params=params)
-            elif method == 'POST':
-                self.client.post(url, headers=headers, params=params, data=post_data)
-            elif method == 'PUT':
-                self.client.put(url, headers=headers, params=params, data=post_data)
-            elif method == 'DELETE':
-                self.client.delete(url, headers=headers, params=params)
+# запуск из директории с файлом locustfile.py
+# locust -f locustfile.py
